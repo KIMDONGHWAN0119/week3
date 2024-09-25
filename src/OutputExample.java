21f12cfa9bf30903947976b85666b7f2d9c30c60
DONGHWAN <mpqd2005@gmail.com>
2024-09-25 16:22:12 +0900

    first

---
+++ src/OutputExample.java
@@ -0,0 +1,10 @@
+public class OutputExample
+{
+    public static void main(String[] args){
+        int age = 22;
+        String sentence = "노는게 제일 좋아";
+        String name = "뽀로로";
+        //System.out.println(name+"\n"+age+"세\n"+sentence);
+        System.out.println(name+"("+age+"세) "+sentence);
+    }
+}
